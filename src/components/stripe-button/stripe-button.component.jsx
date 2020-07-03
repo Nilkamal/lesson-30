925640f7f8575c6c3de41717b8006fb5bf7d2ec8
Nilkamal Shah <nilkamal.shah@publicissapient.com>
2020-07-03 16:16:44 +0530

    Changing firebase config'

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'sk_test_SqvygBAlNsrqcDhKQzmahMqU00pPAq9xBi';
 
   const onToken = token => {
     console.log(token);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,16 +1,17 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-
-const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
-};
+var
+  config = {
+    apiKey: "AIzaSyCeCU4waNMUo0caGy5HYmcxVLqJsh_8xzs",
+    authDomain: "crown-db-c763b.firebaseapp.com",
+    databaseURL: "https://crown-db-c763b.firebaseio.com",
+    projectId: "crown-db-c763b",
+    storageBucket: "crown-db-c763b.appspot.com",
+    messagingSenderId: "656706097150",
+    appId: "1:656706097150:web:2ec84ea68e5af3dde8faaf",
+    measurementId: "G-FB3BY7JVWW"
+  };
 
 firebase.initializeApp(config);
 
@@ -22,7 +23,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   const snapShot = await userRef.get();
 
   if (!snapShot.exists) {
-    const { displayName, email } = userAuth;
+    const {
+      displayName,
+      email
+    } = userAuth;
     const createdAt = new Date();
     try {
       await userRef.set({
@@ -56,7 +60,10 @@ export const addCollectionAndDocuments = async (
 
 export const convertCollectionsSnapshotToMap = collections => {
   const transformedCollection = collections.docs.map(doc => {
-    const { title, items } = doc.data();
+    const {
+      title,
+      items
+    } = doc.data();
 
     return {
       routeName: encodeURI(title.toLowerCase()),
@@ -85,7 +92,9 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
-googleProvider.setCustomParameters({ prompt: 'select_account' });
+googleProvider.setCustomParameters({
+  prompt: 'select_account'
+});
 export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 export default firebase;
